ee1a6ed00aa49811ef3914b3c1f156973d007c99
Kevin <nodgez@hotmail.com>
2015-08-27 11:28:31 +0100

    Event Classes created

---
+++ Assets/Scripts/GameEvent.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public delegate void OnHandleEvent();
+
+public class GameEvent : MonoBehaviour {
+
+	public event OnHandleEvent onHandleEvent;
+	private List<ITrigger> eventTriggers = new List<ITrigger>();
+
+	public void CheckEventTriggered () {
+		foreach (ITrigger eventTrigger in eventTriggers) {
+			if (eventTrigger.IsTriggered) {
+
+				if(onHandleEvent != null)
+					onHandleEvent();
+
+				break;
+			}
+		}
+	}
+
+	public void AddEventTrigger(ITrigger trigger)
+	{
+		eventTriggers.Add (trigger);
+	}
+}
---
+++ Assets/Scripts/Managers/EventPool.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EventPool : MonoBehaviour {
+
+	private Dictionary <string, GameEvent> eventDictionary = new Dictionary<string, GameEvent>();
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		foreach (string s in eventDictionary.Keys)
+			eventDictionary [s].CheckEventTriggered ();
+	}
+
+	public void AddEventToPool(string key, GameEvent gameEvent)
+	{
+		if (!eventDictionary.ContainsKey (key))
+			eventDictionary.Add (key, gameEvent);
+		else
+			eventDictionary [key] = gameEvent;
+	}
+
+	public GameEvent GetEventFromPool(string key)
+	{
+		if (eventDictionary.ContainsKey (key))
+			return eventDictionary [key];
+		else
+			return null;
+	}
+}
