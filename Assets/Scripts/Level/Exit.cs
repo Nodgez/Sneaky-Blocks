cb43fd00b3fbdb42a9500d892009d7a5a22ed761
unknown <percussion505@hotmail.com>
2020-06-24 15:02:06 +0100

    added Easy Mobile pro and imported to latest unity 2019 verson

--- Assets/Scripts/Level/Exit.cs
+++ Assets/Scripts/Level/Exit.cs
@@ -8,29 +8,24 @@ using UnityEngine.SceneManagement;
 [RequireComponent(typeof(ProximityDetector))]
 public class Exit : MonoBehaviour, ICameraMoveTo {
 	private ProximityDetector _detector;
-    float t = 0;
-
+	AsyncOperation _nextLevelLoadOp;
 	public int CameraPriority { get; set; }
 
     void Start()
 	{
 		CameraPriority = 1;
 		_detector = GetComponent<ProximityDetector> ();
+		int levelToLoad = SceneManager.GetActiveScene().buildIndex + 1 < SceneManager.sceneCount ? SceneManager.GetActiveScene().buildIndex + 1 : 0;
+		_nextLevelLoadOp = SceneManager.LoadSceneAsync(levelToLoad);
+		_nextLevelLoadOp.allowSceneActivation = false;
 	}
 
 	void Update()
 	{
-        if (!_detector.DetectTargets())
-            return;
-
-        int levelToLoad = SceneManager.GetActiveScene().buildIndex + 1 < SceneManager.sceneCount ? SceneManager.GetActiveScene().buildIndex + 1 : 0;
-        BackingTrackManager.Instance.EffectsLibrary.PlayClip(AudioEffects.Win);
-        Time.timeScale = 0;
-        t += Time.unscaledDeltaTime;
-        if (t >= 2.2f)
-        {
-            SceneManager.LoadScene(levelToLoad);
-            Time.timeScale = 1;
-        }
+		if (!_detector.DetectTargets())
+			return;
+		BackingTrackManager.Instance.EffectsLibrary.PlayClip(AudioEffects.Win);
+		_nextLevelLoadOp.allowSceneActivation = true;
+		enabled = false;
 	}
 }
