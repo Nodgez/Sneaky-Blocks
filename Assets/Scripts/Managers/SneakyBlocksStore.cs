9ec4e5e2260f662d384426d4ece36191e87d20fc
unknown <percussion505@hotmail.com>
2020-07-04 09:37:03 +0100

    added in firebase and some set up for notifications

--- Assets/Scripts/Managers/SneakyBlocksStore.cs
+++ Assets/Scripts/Managers/SneakyBlocksStore.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using EasyMobile;
 public class SneakyBlocksStore : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public void PurhcaseAdRemoval()
+	{
+		if (!InAppPurchasing.IsInitialized())
+			return;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+		var removeAdProduct = InAppPurchasing.GetIAPProductById("remove_ads");
+		InAppPurchasing.Purchase(removeAdProduct);
+	}
+
+	private void OnEnable()
+	{
+		InAppPurchasing.PurchaseCompleted += InAppPurchasing_PurchaseCompleted;
+		InAppPurchasing.PurchaseFailed += InAppPurchasing_PurchaseFailed;
+		}
+
+	private void InAppPurchasing_PurchaseFailed(IAPProduct product)
+	{
+		Debug.Log("purchased product: " + product.Name + " failed");
+	}
+
+	private void OnDisable()
+	{
+		InAppPurchasing.PurchaseCompleted -= InAppPurchasing_PurchaseCompleted;
+		InAppPurchasing.PurchaseFailed -= InAppPurchasing_PurchaseFailed;
+	}
+
+	private void InAppPurchasing_PurchaseCompleted(IAPProduct product)
+	{
+		Debug.Log("purchased product: " + product.Name);
+	}
 }
